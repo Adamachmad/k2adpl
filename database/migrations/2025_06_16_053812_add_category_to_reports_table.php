082a1d09bf1b36b254a7d0e1137bc2fce1ffe15a
Adamachmad <adamachmad8@gmail.com>
2025-06-16 13:39:22 +0800

    perbaiki kirim laporan

---
+++ database/migrations/2025_06_16_053812_add_category_to_reports_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCategoryToReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reports', function (Blueprint $table) {
+            $table->string('category')->nullable()->after('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reports', function (Blueprint $table) {
+            $table->dropColumn('category');
+        });
+    }
+}
